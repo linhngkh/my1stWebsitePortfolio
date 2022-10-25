6a5954a9ffc6fd87e986d0ccaac623301d8a9651
Linh <linh.nguyenkhanhib@gmail.com>
2022-10-25 19:00:55 +0300

    adding vanta.js particle

---
+++ vanta.js
@@ -0,0 +1,14 @@
+VANTA.NET({
+  el: "#animation-canvas",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: '#a37ef8',
+  points: 10.00,
+  maxDistance: 20.00,
+  spacing: 12.00
+})
