682397544185e382b98a0bc918e671dceeb027b6
Linh <linh.nguyenkhanhib@gmail.com>
2022-11-15 22:16:12 +0200

    change some style css

--- vanta.js
+++ vanta.js
@@ -3,12 +3,12 @@ VANTA.NET({
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  scale: 1.00,
-  scaleMobile: 1.00,
-  color: '#a37ef8',
-  points: 10.00,
-  maxDistance: 20.00,
-  spacing: 12.00
-})
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: "#a37ef8",
+  points: 10.0,
+  maxDistance: 20.0,
+  spacing: 12.0,
+});
